dbf00d3083f4c268a9714f01e6be61ddfe704ca4
ashinzekene <ashinzekene@gmail.com>
2018-02-25 14:58:23 +0100

    pins route

--- routes/middlewares.js
+++ routes/middlewares.js
@@ -4,6 +4,13 @@ function adminCategory(req, res, next) {
     res.status(401).json({ err: "Why would you even try that, thief!" })
 }
 
+function adminPin(req, res, next) {
+  req.user._id === req.pin.user ?
+    next() :
+    res.status(401).json({ err: "Why would you even try that, thief!" })
+}
+
 module.exports = {
-  adminCategory
+  adminCategory,
+  adminPin,
 }
